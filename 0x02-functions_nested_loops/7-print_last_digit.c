3abcb5ab9d88eeca97395109b1363751c3e7ed29
Woruka Onward <worukaonward@gmail.com>
2022-10-18 12:00:45 -0700

    commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,13 +9,12 @@ int print_last_digit(int n)
 	int a;
 
 	if (n < 0)
-		n = -n;
-	a = n % 10;
+		n = -1 * (n % 10);
 
-	if (a < 0)
-		a = -a;
+	else
+		a = n % 10;
 
-	_putchar(a + '0');
+	_putchar((a % 10) + '0');
 
-	return (a);
+	return (a % 10);
 }
